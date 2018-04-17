385233a19a17d433b8e5fac3b6421c50c8c67545
Sheena Tumblin <sqt2516@yahoo.com>
2018-04-17 14:47:10 -0500

    Updated controller views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
+    before_action :find message, only: [:create, :edit, :update, :destroy]
+    before_action :find comment, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!
 
     def create
-        @message = Message.find(params[:message_id])
         @comment = @message.comments.create(comment_params)
         @comment.user_id = current_user.id
 
@@ -12,10 +14,32 @@ class CommentsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @comment.update(comment_params)
+            redirect_to message_path(@message)
+        else
+            render 'edit'
+        end
+
+    def destroy
+        @comment.destroy
+        redirect_to message_path(@message)
+    end
+
+
     private 
 
     def comment_pararms
         params.require(:comment).permit(:content)
     end
 
-end
+    def find_message
+        @message = Message.find(params[:message_id])
+    end
+
+    def find_comment
+        @comment = @message.comment.find(params[:id])
+    end
